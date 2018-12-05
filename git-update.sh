a4710d18c14a707bf9b8cb586fa1518a61693483
FoolishChow <ningjingsiyuan1990@163.com>
2018-12-05 10:14:48 +0800

    Update git-update.sh

--- git-update.sh
+++ git-update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-yum install -y tk zlib-devel openssl-devel perl cpio expat-devel gettext-devel asciidoc xmlto
+yum install -y gcc gcc-c++ tk zlib-devel openssl-devel perl cpio expat-devel gettext-devel asciidoc xmlto
+yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel -y
+yum install gcc perl-ExtUtils-MakeMaker -y
+
 yum remove git -y
 wget --no-check-certificate https://www.kernel.org/pub/software/scm/git/git-2.12.2.tar.gz
 tar -zxf git-2.12.2.tar.gz
