ed58fbdbdd22348cbfd8850c5725f3b75baf0657
FoolishChow <ningjingsiyuan1990@163.com>
2018-12-05 10:11:23 +0800

    Create git-update.sh

---
+++ git-update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+yum install -y tk zlib-devel openssl-devel perl cpio expat-devel gettext-devel asciidoc xmlto
+yum remove git -y
+wget --no-check-certificate https://www.kernel.org/pub/software/scm/git/git-2.12.2.tar.gz
+tar -zxf git-2.12.2.tar.gz
+cd git-2.12.2
+./configure --prefix=/usr/local/git && make install
+echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/profile
+source /etc/profile
+git --version
